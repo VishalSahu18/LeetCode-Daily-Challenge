5b58260c757e554f89f9ddc7e5f8cb08db6d6341
Vishal Sahu <vishalsahu1827@gmail.com>
2024-09-26 22:35:06 +0530

    cpp solution added

---
+++ 729. My Calendar I/solution.cpp
@@ -0,0 +1,28 @@
+class MyCalendar {
+public:
+    
+    set<pair<int,int>>st;
+    
+    MyCalendar() {
+        
+     } 
+    
+    bool book(int start, int end) {
+       
+        auto it  = st.lower_bound({start,end});
+        
+        if(it!=st.end() && !(end<=it->first) || it!=st.begin() && !(prev(it)->second<=start))
+            return false;
+        
+        
+        st.insert({start,end});
+        return true;
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
