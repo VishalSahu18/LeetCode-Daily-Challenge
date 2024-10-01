b5431bc086f0efc6be30ac4f6dae76f17c305190
Vishal Sahu <vishalsahu1827@gmail.com>
2024-10-02 02:29:57 +0530

    python solution modified

--- 1331. Rank Transform of an Array/solution.py
+++ 1331. Rank Transform of an Array/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         n = len(arr)
-        hm = {}
+        map = {}
         nums = arr[:]
 
         nums.sort()
         rank = 1
 
         for num in nums:
-            if num not in hm:
-                hm[num] = rank
+            if num not in map:
+                map[num] = rank
                 rank += 1
 
-        return [hm[num] for num in arr]
+        return [map[num] for num in arr]
